9dc0bcb892b9c03e9ae539ef791e8b5fb2a8eccb
Mauricio Cadenas <morris.cadenas@gmail.com>
2025-10-01 19:37:49 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // IMPORTANT: Replace REPO_NAME below with your GitHub repo name (case-sensitive), e.g. 'dnd-combat-manager'
 export default defineConfig({
-  plugins: [react()],
-  base: '/dnd-combat-manager/'
-})
+  plugins: [react(), tailwindcss()],
+  base: '/dnd-combat-manager/',
+});
